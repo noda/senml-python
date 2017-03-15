288778da666a34a2fb40a00f88aecc2e61f636aa
Carl Fürstenberg <azatoth@gmail.com>
2017-03-15 15:04:40 +0100

    adding iso8601 field

--- tests/test_senml.py
+++ tests/test_senml.py
@@ -31,6 +31,11 @@ JS = json.loads('''
     "n":"str",
     "vs":"hej",
     "t":2.0
+},
+{
+    "n":"isotime",
+    "vs":"då",
+    "iso8601":"2017-03-15T15:00:08+01:00"
 }
 ]
 ''')
@@ -71,6 +76,13 @@ def test_senmlmeasurement_to_json():
     assert js_out == JS[1]
 
 
+def test_senmlmeasurement_iso8601_to_json():
+    """test SenMLMeasurement.to_json"""
+    meas = senml.SenMLMeasurement.from_json(JS[2])
+    js_out = meas.to_json()
+    assert js_out == JS[2]
+
+
 def test_senmldocument_to_json():
     """test SenMLDocument.to_json"""
     doc = senml.SenMLDocument.from_json(JS)
@@ -89,6 +101,43 @@ def test_senmlmeas_to_absolute():
     assert float_almost_equal(mabs.sum, 4)
     assert float_almost_equal(mabs.time, 1234567897.123)
 
+
+JS_ISOTIME = json.loads('''
+[
+{
+    "biso8601":"2017-03-15T15:00:00+01:00",
+    "bn":"urn:dev:mac:0b92569229fc9e68/rpm/",
+    "v":17.666544
+},
+{
+    "n":"isotime1",
+    "vs":"α"
+},
+{
+    "iso8601":"2017-03-14T00:00:00+01:00",
+    "n":"isotime2",
+    "vs":"β"
+}
+
+]
+''')
+
+ABS_JSON_ISOTIME = [{
+    "iso8601": "2017-03-15T15:00:00+01:00",
+    "n": "urn:dev:mac:0b92569229fc9e68/rpm/",
+    "t": 0.0,
+    "v": 17.666544
+}, {
+    "iso8601": "2017-03-15T15:00:00+01:00",
+    "n": "urn:dev:mac:0b92569229fc9e68/rpm/isotime1",
+    "t": 0.0,
+    "vs": u"α"
+}, {
+    "iso8601": "2017-03-14T00:00:00+01:00",
+    "n": "urn:dev:mac:0b92569229fc9e68/rpm/isotime2",
+    "t": 0.0,
+    "vs": u"β"
+}]
 
 ABS_JSON = [{
     "s": 3.0,
@@ -112,6 +161,12 @@ ABS_JSON = [{
     "u": "1/min",
     "t": 1234567892.123,
     "n": "urn:dev:mac:0b92569229fc9e68/rpm/str"
+}, {
+    "iso8601": "2017-03-15T15:00:08+01:00",
+    "n": "urn:dev:mac:0b92569229fc9e68/rpm/isotime",
+    "t": 1234567890.123,
+    "u": "1/min",
+    "vs": u"då"
 }]
 
 
@@ -121,3 +176,11 @@ def test_senml_normalize_json():
     """
     doc = senml.SenMLDocument.from_json(JS)
     assert doc.to_normalized_json() == ABS_JSON
+
+
+def test_iso8601():
+    """
+    test SenMLDocument.to_normalized_json() with iso8601 field
+    """
+    doc = senml.SenMLDocument.from_json(JS_ISOTIME)
+    assert doc.to_normalized_json() == ABS_JSON_ISOTIME
